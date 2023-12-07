34f766e49c8b29f58f987712ae455aa577f2352a
Seungheon Oh <seungheon.ooh@gmail.com>
2023-12-07 15:25:15 -0600

    In the beginning there was darkness

---
+++ init.el
@@ -0,0 +1,16 @@
+(load-file "~/.emacs.d/configuration.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("bfc0b9c3de0382e452a878a1fb4726e1302bf9da20e69d6ec1cd1d5d82f61e3d" "a8970b307ece3e37d7d56df11a7199733793b6a8482e1010dc40b027b4994183" default))
+ '(purescript-mode-hook '(turn-on-purescript-simple-indent))
+ '(warning-suppress-types '((comp))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
